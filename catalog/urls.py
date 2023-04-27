3aede1bbbaad9aa444d770e0da5347071b8c5641
thai-ru <robinthairu34@gmail.com>
2023-04-27 15:42:45 +0300

    Work on Views and Index Page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-
+urlpatterns += [
+    path('', views.index, name='index'),
+    path('catalog/', include('catalog.urls')),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from .models import Author, Books, Genre, BookInstance
 # Create your views here.
